09b7c52be28e30612629370bb228c76422e40ec6
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2021-11-30 15:57:16 +0100

    initial commit

---
+++ src/main/java/net/reqium/coinssystem/CoinsSystem.java
@@ -0,0 +1,17 @@
+package net.reqium.coinssystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoinsSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        init();
+    }
+
+
+    public void init() {
+
+    }
+
+}
